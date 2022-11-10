ba0128a9042d3dc78c1ca5f4f4c3763570f998f3
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-11-10 12:34:12 -0500

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function show(num){
+    if(num==1){
+        document.getElementById("text").style.display="none";
+    }
+    else if(num == 2){
+        document.getElementById("text").style.display="block";
+    }
+}
